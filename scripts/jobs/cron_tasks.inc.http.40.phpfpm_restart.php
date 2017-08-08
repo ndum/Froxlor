38c7615fb595e68fc7e0ef0689c713f5fefe7cc2
Nicolas D <nd@nidum.org>
2017-08-09 00:23:28 +0200

    PHP-FPM reload bugfix
    - Rewritten the SQL.Query
    - Fixed a bug which occur on config changes
    - Improved reloading & performance

--- scripts/jobs/cron_tasks.inc.http.40.phpfpm_restart.php
+++ scripts/jobs/cron_tasks.inc.http.40.phpfpm_restart.php
@@ -9,12 +9,12 @@
  * COPYING file online at http://files.froxlor.org/misc/COPYING.txt
  *
  * @copyright  (c) the authors
- * @author     Michael Kaufmann <mkaufmann@nutime.de>
+ * @author     Nicolas D <nd@nidum.org>
  * @author     Froxlor team <team@froxlor.org> (2010-)
  * @license    GPLv2 http://files.froxlor.org/misc/COPYING.txt
  * @package    Cron
  *
- * @since      0.9.29.1
+ * @since      0.9.37
  *
  */
 
@@ -24,36 +24,41 @@ class phpfpm_restart
 	/*
 	 * Froxlor's $cronlog
 	 */
-	private $logger = array( );
+	private $logger = array();
 	
 	/*
 	 * All required run-scripts
 	 */
-	private $runscripts = array( );
+	private $runscripts = array();
 	
 	public function __construct($logger) {
 		$this->logger = $logger;
 		
-		$result_runscripts_stmt = Database::query("
-			SELECT `runscript` FROM `" . TABLE_PANEL_PHPCONFIGS . "` phpconfig
-			WHERE EXISTS(SELECT * FROM `" . TABLE_PANEL_DOMAINS . "` as domain WHERE domain.phpsettingid = phpconfig.id)
-		");
-		while ($row = $result_runscripts_stmt->fetch(PDO::FETCH_ASSOC)) {
-			$this->runscripts[$row['runscript']] = true;
+		$result_get_runscripts_stmt = Database::query("
+			SELECT DISTINCT `runscript` FROM`" . TABLE_PANEL_PHPCONFIGS . "`");
+		while ($row = $result_get_runscripts_stmt->fetch(PDO::FETCH_ASSOC)) {
+			if($row['runscript'] == '') {
+				continue;
+			}
+			$this->runscripts[] = array($row['runscript']);
 		}
+		$this->runscripts[] = array(substr(Settings::Get('phpfpm.reload'), 0, -8));
 	}
 	
 	public function restart() {
 		$this->logger->logAction(CRON_ACTION, LOG_NOTICE, 'restarting php-fpm');
-		$this->executeRunScripts("stop");
-		$this->executeRunScripts("start");
-	}
-	
-	public function executeRunScripts($command) {
-		foreach($this->runscripts as $runscript => $useless) {
-			$this->logger->logAction(CRON_ACTION, LOG_NOTICE, "Running: {$runscript} {$command}");
-			safe_exec("{$runscript} {$command}");
+		foreach($this->runscripts as $outerarray) {
+			foreach($outerarray as $runscript) {
+				safe_exec("{$runscript} stop");
+			}
 		}
+
+		foreach($this->runscripts as $outerarray) {
+			foreach($outerarray as $runscript) {
+				safe_exec("{$runscript} start");
+			}
+		}
+
 	}
 	
 }
